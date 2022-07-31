e4a6714365d87a5946d5550ba65b52bf1b2cf87d
MD AL JABIR <aljabir098@gmail.com>
2022-07-31 14:40:52 +0600

    add js

---
+++ app.js
@@ -0,0 +1,7 @@
+const billTotalInput = document.getElementById('billTotalInput');
+
+const tipInput = document.getElementById('tipInput');
+
+const perPersonTotal = document.getElementById('perPersonTotal');
+
+// console.log(billTotalInput, tipInput, perPersonTotal);
